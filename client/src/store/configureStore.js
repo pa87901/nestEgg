094cc8b15dd619b05940969e8d0490d0e0cf419b
Preda Anuvatnujotikul <preda.anu@gmail.com>
2018-02-06 23:04:32 -0800

    Added a Redux store.

---
+++ client/src/store/configureStore.js
@@ -0,0 +1,19 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { createBrowserHistory } from 'history';
+import { routerMiddleware } from 'react-router-redux';
+import rootReducer from '../reducers/index';
+
+const logger = createLogger();
+const history = createBrowserHistory();
+const historyMiddleware = routerMiddleware(history);
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk, logger, historyMiddleware),
+    typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
+  )
+);
+
+export { store, history };
