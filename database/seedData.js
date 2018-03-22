e5001ad33cab5ffa89e8f885892f34a9f71cee17
Preda Anuvatnujotikul <preda.anu@gmail.com>
2018-03-21 20:00:23 -0700

    Rewrote the seeding database script to empty and reseed the transactions table as well as the holdings table.

--- database/seedData.js
+++ database/seedData.js
@@ -17,13 +17,19 @@ const valuesTransactions = pgp.helpers.insert(values, csTransactions);
 const seedData = () => (
   db.none('TRUNCATE holdings RESTART IDENTITY CASCADE')
   .then(() => {
-    console.log('All tables have been emptied.'); // eslint-disable-line no-console
+    console.log('holdings table emptied. Now seeding with dummy holdings data.'); // eslint-disable-line no-console
     return db.none(valuesHoldings);
   })
-  .then(() => db.none(valuesTransactions))
   .then(() => {
-    console.log('holdings table seeded.');
-    console.log('Data seeding completed.');
+    console.log('holdings table seeded.'); // eslint-disable-line no-console
+    return db.none('TRUNCATE transactions RESTART IDENTITY CASCADE');
+  })
+  .then(() => {
+    console.log('transactions table has been emptied. Now seeding with dummy transactions data.'); // eslint-disable-line no-console
+    return db.none(valuesTransactions);
+  })
+  .then(() => {
+    console.log('Data seeding completed.'); // eslint-disable-line no-console
   })
   .catch(err => console.error('Data seeding encountered an error:', err)) // eslint-disable-line no-console
 );
