2f7c68e320a34302fedbe585eb3fd539bcf5ffa4
Preda Anuvatnujotikul <preda.anu@gmail.com>
2018-04-20 21:29:02 -0700

    More cleanups.

--- server/controllers/holdingsMongo.js
+++ server/controllers/holdingsMongo.js
@@ -1,58 +1,13 @@
 const express = require('express');
-
-const router = express.Router();
-
-/*
-const { Holdings, Transactions } = require('../../database-mongodb/models');
-
-router.get('/', (req, res) => {
-  Holdings.find()
-  .then(holdings => {
-    // console.log('Holdings from mongodb:', holdings);
-    res.status(200).send(JSON.stringify(holdings));
-  })
-  .catch(err => {
-    console.error('Unable to get holdings from mongodb.', err);
-    res.status(500).send([]);
-  });
-});
-
-router.get('/:symbol', (req, res) => {
-  const { symbol } = req.params;
-  Holdings.findOne({ symbol })
-  .then(response => {
-    // console.log('MONGO symbol response:', response);
-    res.status(200).send(response);
-  })
-  .catch(err => {
-    console.error(`Unable to find a holding with symbol ${symbol}.`, err);
-    res.status(500).send(err);
-  });
-});
-
-router.delete('/', (req, res) => {
-  const { selected } = req.body;
-  // console.log('Selected holdings to delete:', selected);
-  Holdings.remove({ symbol: { $in: selected } })
-  .then(() => Transactions.remove({ symbol: { $in: selected } }))
-  .then(response => {
-    const responseBody = {
-      selected,
-      response
-    }
-    res.status(418).send(responseBody);
-  })
-  .catch(err => {
-    console.error('error deleting symbols', err);
-    res.status(500).send('NO SIR');
-  });
-});
-*/
-
-const Promise = require('bluebird');
-const { getAllHoldings, getOneHolding, deleteHoldings } = require('../../database-mongodb/models/holdings');
+const {
+  getAllHoldings,
+  getOneHolding,
+  deleteHoldings
+} = require('../../database-mongodb/models/holdings');
 const { deleteTransactions } = require('../../database-mongodb/models/transactions');
 
+
+const router = express.Router();
 
 router.get('/', (req, res) => {
   getAllHoldings()
@@ -83,7 +38,11 @@ router.delete('/', (req, res) => {
   deleteHoldings(selected)
   .then(() => deleteTransactions(selected))
   .then(response => {
-    res.status(200).send(response);
+    const payload = {
+      response,
+      selected
+    }
+    res.status(303).send(payload);
   })
   .catch(err => {
     console.error('error deleting symbols', err);
--- server/controllers/transactionsMongo.js
+++ server/controllers/transactionsMongo.js
@@ -1,27 +1,11 @@
 const express = require('express');
+const {
+  getAllTransactions,
+  deleteTransactions
+} = require('../../database-mongodb/models/transactions');
+
 
 const router = express.Router();
-
-/*
-const { Transactions } = require('../../database-mongodb/models');
-
-router.get('/', (req, res) => {
-  Transactions.find()
-  .then(transactions => {
-    // console.log('Transactions from mongodb:', transactions);
-    res.status(200).send(JSON.stringify(transactions));
-  })
-  .catch(err => {
-    console.error('Unable to get transactions from mongodb.', err);
-    res.status(500).send([]);
-  });
-});
-*/
-
-const { getAllTransactions, deleteTransactions } = require('../../database-mongodb/models/transactions');
-
-// getAll = Promise.promisify(getAll);
-// deleteTransactions = Promise.promisify(deleteTransactions);
 
 router.get('/', (req, res) => {
   getAllTransactions()
